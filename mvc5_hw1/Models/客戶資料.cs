d442245a3b7149e42080ca01002a3bbbb8a3a35f
ming-wong <ming_wong@lativ.com.tw>
2015-07-10 17:29:50 +0800

    hw1客戶資料新增驗證，排除DB

--- mvc5_hw1/Models/客戶資料.cs
+++ mvc5_hw1/Models/客戶資料.cs
@@ -11,6 +11,7 @@ namespace mvc5_hw1.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class 客戶資料
     {
@@ -21,11 +22,18 @@ namespace mvc5_hw1.Models
         }
     
         public int Id { get; set; }
+        [StringLength(20)]
+        [MinLength(5)]
+        [Required(ErrorMessage="客戶名稱必填")]
         public string 客戶名稱 { get; set; }
+        [Required(ErrorMessage = "統一編號必填")]
         public string 統一編號 { get; set; }
+        [Required(ErrorMessage = "電話必填")]
         public string 電話 { get; set; }
         public string 傳真 { get; set; }
         public string 地址 { get; set; }
+        [Required(ErrorMessage = "Email必填")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
     
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
