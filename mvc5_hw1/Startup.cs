9115fff14483bdbd6794baf7a4b1e9f05332ad1b
ming-wong <ming_wong@lativ.com.tw>
2015-07-10 10:46:26 +0800

    upload

---
+++ mvc5_hw1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc5_hw1.Startup))]
+namespace mvc5_hw1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
